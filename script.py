ab54d7d9d3ad1c162d515386f0b5bdb3fad49265
Gilson Kedson <gilsonkedson15@outlook.com>
2025-03-17 19:09:26 -0300

    refactor: melhorias no script

--- script.py
+++ script.py
@@ -2,26 +2,12 @@ import pandas as pd
 
 enem_result = pd.read_excel("planilhas/resultado_enem.xlsx")
 titles_result = pd.read_excel("planilhas/titulos_20220204.xlsx")
-participants = {
-    'result': []
-}
 
-for x in range(titles_result['NOME'].count()):
-    for y in range(enem_result['NOME'].count()):
-        if titles_result['NOME'][x] == enem_result['NOME'][y]:
-            final_score = (0.6 * enem_result['NOTA'][y]) + (0.4 * titles_result['NOTA'][x])
-            participants['result'].append([titles_result['NOME'][x], final_score])
-            
-def order(participants):
-    for passnum in range(len(participants)-1,0,-1):
-        for i in range(passnum):
-            if participants[i][1]<participants[i+1][1]:
-                aux = participants[i]
-                participants[i] = participants[i+1]
-                participants[i+1] = aux
+merged_df = pd.merge(titles_result, enem_result, on="NOME", suffixes=('_titulos', '_enem'))
 
-result = participants['result']
-order(result)
+merged_df["NOTA_FINAL"] = (0.6 * merged_df["NOTA_enem"]) + (0.4 * merged_df["NOTA_titulos"])
 
-for r in range(1, len(result)+1):
-    print(f'Posição {r} - NOME: {result[r-1][0]} - NOTA: {round(result[r-1][1], 2)}')
+sorted_df = merged_df.sort_values(by="NOTA_FINAL", ascending=False).reset_index(drop=True)
+
+for idx, row in sorted_df.iterrows():
+    print(f'Posição {idx+1} - NOME: {row["NOME"]} - NOTA: {round(row["NOTA_FINAL"], 2)}')
